e1a09656556d9f3988e3612b40f2c2e399921985
squium <helyotjeff@gmail.com>
2023-10-10 12:56:05 +0200

    Ajout des exos 7

---
+++ exo7 autre/main.js
@@ -0,0 +1,5 @@
+let names = [];
+
+names.push("Vincent", "Paul", "Arthur");
+
+names.forEach((nom) => console.log(nom, "va à la pêche"));
---
+++ exo7/main.js
@@ -0,0 +1,8 @@
+let names = [];
+
+names.push("Vincent", "Paul", "Arthur");
+
+names.forEach(nom => {
+  nom += " va à la pêche"
+  console.log(nom);
+});
