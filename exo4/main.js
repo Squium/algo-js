170970536f305253821194521ce65f94b95e8a70
squium <helyotjeff@gmail.com>
2023-10-10 11:07:14 +0200

    2e modif Ajout des exo 4, 5 et 6

--- exo4/main.js
+++ exo4/main.js
@@ -1,5 +1,12 @@
-let a = 11
+let a = 11;
 
 for (let i = 0; i < a; i++) {
   console.log("oklm");
+}
+
+let u = 0;
+
+while (u < 10) {
+  console.log(u);
+  u++;
 }
---
+++ exo6/main.js
@@ -0,0 +1,9 @@
+let a = "Jean"; 
+let b = "Paul"; 
+
+function checkName (Nom1, Nom2) {
+  return Nom1 == Nom2
+}
+
+let result = checkName(a, b); 
+console.log(result);
